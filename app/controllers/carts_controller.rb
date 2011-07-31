984a281a068311e33874e9a56d036ff127f5147c
Ian's Ubuntu <magicbacon@ubuntu.(none)>
2011-07-30 22:08:09 -0400

    Depot Scaffold

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -78,12 +78,11 @@ class CartsController < ApplicationController
   # DELETE /carts/1.xml
   def destroy
     @cart = current_cart
-    @cart.destroy
+    @cart.destroy 
     session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to(store_url,
-        :notice => 'Your cart is currently empty') }
+      format.html { redirect_to(store_url) }
       format.xml  { head :ok }
     end
   end
